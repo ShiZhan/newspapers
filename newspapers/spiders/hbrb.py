b82b14ccf1c3ecf8bf5a311a70dd05a5b91b184f
ShiZhan <g.shizhan.g@gmail.com>
2012-11-17 03:38:24 +0800

    initial framework

---
+++ newspapers/spiders/hbrb.py
@@ -0,0 +1,21 @@
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from newspapers.items import NewspapersItem
+
+class HbrbSpider(CrawlSpider):
+    name = 'hbrb'
+    allowed_domains = ['hbrb.cnhubei.com']
+    start_urls = ['http://www.hbrb.cnhubei.com/']
+
+    rules = (
+        Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        hxs = HtmlXPathSelector(response)
+        i = NewspapersItem()
+        #i['domain_id'] = hxs.select('//input[@id="sid"]/@value').extract()
+        #i['name'] = hxs.select('//div[@id="name"]').extract()
+        #i['description'] = hxs.select('//div[@id="description"]').extract()
+        return i
