f5d27c5fa223f22f662679ed2e9936ff5a3ae7b7
ShiZhan <g.shizhan.g@gmail.com>
2012-11-26 15:54:17 +0800

    fix whwb spider.

--- newspapers/spiders/whwb.py
+++ newspapers/spiders/whwb.py
@@ -13,7 +13,7 @@ class WhwbSpider(CrawlSpider):
     # start_urls = ['http://cjmp.cnhan.com/whwb/html/2012-11/20/node_22.htm']
 
     rules = (
-        # Rule(SgmlLinkExtractor(allow=r'content_[0-9]+.htm'), callback='parse_item', follow=True), this also works
+        Rule(SgmlLinkExtractor(restrict_xpaths='/html/body/a', allow=r'node_[0-9]{2,3}.htm'), follow=True),
         Rule(SgmlLinkExtractor(restrict_xpaths='//a[@id=\'pageLink\']', allow=r'node_[0-9]{2,3}.htm'), follow=True),
         Rule(SgmlLinkExtractor(restrict_xpaths='//a/div[@id]/..', allow=r'content_[0-9]+.htm'), callback='parse_item', follow=False),
     )
