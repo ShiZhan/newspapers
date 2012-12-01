ff1df623e1b651643e81e1f2fc7c429399fc621f
ShiZhan <g.shizhan.g@gmail.com>
2012-12-01 14:48:55 +0800

    add some information to escaped-unicode converting script.

--- utils/escaped-unicode-print.py
+++ utils/escaped-unicode-print.py
@@ -1,6 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 import sys
 
+if len(sys.argv) < 2:
+    print "where is input escaped-unicode file?"
+    exit(1)
+
 with open(sys.argv[1]) as f:
-  str = f.read().decode('unicode_escape')
-  print str.encode('utf-8')
+    str = f.read().decode('unicode_escape')
+    print str.encode('utf-8')
